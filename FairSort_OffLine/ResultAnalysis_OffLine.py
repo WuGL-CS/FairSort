f7bf70dd3acb11a70551c87d2504aa707edbc3c0
13543024276 <13543024276@163.com>
2023-05-30 17:29:56 +0800

    Final Version

--- FairSort_OffLine/ResultAnalysis_OffLine.py
+++ FairSort_OffLine/ResultAnalysis_OffLine.py
@@ -1,6 +1,35 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
+class DraggableLegend:
+    def __init__(self, legend):
+        self.legend = legend
+        self.gotLegend = False
+        legend.figure.canvas.mpl_connect('motion_notify_event', self.on_motion)
+        legend.figure.canvas.mpl_connect('pick_event', self.on_pick)
+        legend.figure.canvas.mpl_connect('button_release_event', self.on_release)
+        legend.set_picker(self.my_legend_picker)
+    def on_motion(self, evt):
+        if self.gotLegend:
+            dx = evt.x - self.mouse_x
+            dy = evt.y - self.mouse_y
+            loc_in_canvas = self.legend_x + dx, self.legend_y + dy
+            loc_in_norm_axes = self.legend.parent.transAxes.inverted().transform_point(loc_in_canvas)
+            self.legend._loc = tuple(loc_in_norm_axes)
+            self.legend.figure.canvas.draw()
+    def my_legend_picker(self, legend, evt):
+        return self.legend.legendPatch.contains(evt)
+    def on_pick(self, evt):
+        if evt.artist == self.legend:
+            bBox = self.legend.get_window_extent()
+            self.mouse_x = evt.mouseevent.x
+            self.mouse_y = evt.mouseevent.y
+            self.legend_x = bBox.xmin
+            self.legend_y = bBox.ymin
+            self.gotLegend = 1
+    def on_release(self, event):
+        if self.gotLegend:
+            self.gotLegend = False
 def resultAnalysis_Online(BestResultFilePath,title,indexName,X_len,Y_len,x_label,y_label,marker):
     Y_dict={}
     X = [x for x in range(2,26)]
@@ -14,32 +43,50 @@ def resultAnalysis_Online(BestResultFilePath,title,indexName,X_len,Y_len,x_label
 def paint(X,Y_dict,title,X_len,Y_len,x_label,y_label,marker):
     fig, ax = plt.subplots(figsize=(X_len, Y_len), layout='constrained')
     for modelName, Y in Y_dict.items():
-        ax.plot(X, Y, label=modelName ,linestyle=':',marker=marker)  # Plot more data on the axes...
-    ax.set_xlabel(x_label)  # Add an x-label to the axes.
-    ax.set_ylabel(y_label)  # Add a y-label to the axes.
+        if (modelName=="minimum_exposure"):
+            ax.plot(X, Y, label=modelName ,linestyle=':',marker=marker,color="darkviolet")# Plot more data on the axes...
+        elif(modelName=="Top-K"):
+            ax.plot(X, Y, label=modelName, linestyle=':', marker=marker, color="black")  # Plot more data on the axes...
+        elif(modelName=="FairSort_offline_Quality_Weighted"):
+            ax.plot(X, Y, label=modelName, linestyle=':', marker=marker, color="blue")# Plot more data on the axes...
+        elif (modelName == "FairSort_offline_Uniform"):
+            ax.plot(X, Y, label=modelName, linestyle=':', marker=marker, color="green")  # Plot more data on the axes...
+        elif (modelName == "all_random"):
+            ax.plot(X, Y, label=modelName, linestyle=':', marker=marker, color="purple")  # Plot more data on the axes...
+        elif (modelName == "Mixed-k"):
+            ax.plot(X, Y, label=modelName, linestyle=':', marker=marker, color="maroon")  # Plot more data on the axes...
+        elif (modelName == "Fair_Rec"):
+            ax.plot(X, Y, label=modelName, linestyle=':', marker=marker, color="olive")  # Plot more data on the axes...
+        elif (modelName == "TFROM_offline_Uniform"):
+            ax.plot(X, Y, label=modelName, linestyle=':', marker=marker, color="deeppink")  # Plot more data on the axes...
+        elif (modelName == "TFROM_offline_Quality_Weighted"):
+            ax.plot(X, Y, label=modelName, linestyle=':', marker=marker, color="orange")  # Plot more data on the axes...
+
+    ax.set_xlabel(x_label, fontsize="24")  # Add an x-label to the axes.
+    ax.set_ylabel(y_label, fontsize="24")  # Add a y-label to the axes.
     ax.set_title(title)  # Add a title to the axes.
-    ax.legend()  # Add a legend.
+    DraggableLegend(ax.legend(fontsize="22")) # Add a legend.
     ax.grid(True)
     plt.show()
 
-# # Ctrip:Result_Analysis:
-# #     Total recommendation quality
-# # pd.read_csv("../datasets/results/result_ctrip/FairSortUniformOff_16_1_0.85.csv")
+# Ctrip:Result_Analysis:
+#     Total recommendation quality
+# pd.read_csv("../datasets/results/result_ctrip/FairSortUniformOff_16_1_0.85.csv")
 # BestResultFilePath={}
 # BestResultFilePath["minimum_exposure"]="../BaseLine/Results/OffLine/ctrip/minimumExposure_OffLine.csv"
 # BestResultFilePath["Top-K"]="../BaseLine/Results/OffLine/ctrip/Top_K_Offline.csv"
+# BestResultFilePath["FairSort_offline_Quality_Weighted"]="../datasets/results/result_ctrip/FairSortQualityOff_16_1_0.9.csv"
+# BestResultFilePath["FairSort_offline_Uniform"]="../datasets/results/result_ctrip/FairSortUniformOff_16_1_0.85.csv"
 # BestResultFilePath["all_random"]="../BaseLine/Results/OffLine/ctrip/Random_k_Offline.csv"
 # BestResultFilePath["Mixed-k"]="../BaseLine/Results/OffLine/ctrip/Mixed_k_OffLine.csv"
 # BestResultFilePath["Fair_Rec"]="../BaseLine/Results/OffLine/ctrip/FairRecOffLine.csv"
-# BestResultFilePath["FairSort_offline_Uniform"]="../datasets/results/result_ctrip/FairSortUniformOff_16_1_0.85.csv"
-# BestResultFilePath["FairSort_offline_Quality_Weighted"]="../datasets/results/result_ctrip/FairSortQualityOff_16_1_0.9.csv"
 # BestResultFilePath["TFROM_offline_Quality_Weighted"]="../datasets/results/result_ctrip/TFROM/result_analyze_TFORM_Quality_Offline.csv"
 # BestResultFilePath["TFROM_offline_Uniform"]="../datasets/results/result_ctrip/TFROM/result_analyze_TFROM_Uniform_Offline.csv"
-#
+# #
 # resultAnalysis_Online(BestResultFilePath,"","satisfaction_total",5.8,3.2,"K","total recommendation quality","*")
 
 # Ctrip:Result_Analysis:
-#     # Variance of NDCG
+    # Variance of NDCG
 # BestResultFilePath={}
 # BestResultFilePath["Fair_Rec"]="../BaseLine/Results/OffLine/ctrip/FairRecOffLine.csv"
 # BestResultFilePath["TFROM_offline_Quality_Weighted"]="../datasets/results/result_ctrip/TFROM/result_analyze_TFORM_Quality_Offline.csv"
@@ -111,7 +158,7 @@ def paint(X,Y_dict,title,X_len,Y_len,x_label,y_label,marker):
 # BestResultFilePath["Fair_Rec"]="../BaseLine/Results/OffLine/amazon/FairRecOffLine.csv"
 # BestResultFilePath["TFROM_offline_Quality_Weighted"]="../datasets/results/result_amazon/TFROM/result_quality.csv"
 # BestResultFilePath["TFROM_offline_Uniform"]="../datasets/results/result_amazon/TFROM/result_Uniform.csv"
-#
+
 # resultAnalysis_Online(BestResultFilePath,"","satisfaction_var",5.8,3.2,"K","Variance of NDCG","x")
 
 # Amazon:Result_Analysis:
@@ -140,7 +187,7 @@ def paint(X,Y_dict,title,X_len,Y_len,x_label,y_label,marker):
 # BestResultFilePath["TFROM_offline_Quality_Weighted"]="../datasets/results/result_amazon/TFROM/result_quality.csv"
 #
 # resultAnalysis_Online(BestResultFilePath,"","exposure_quality_var",5.8,3.2,"K","Variance of the ratio of exposure and relevance","1")
-
+#
 
 
 # # Google:Result_Analysis:
@@ -152,10 +199,10 @@ def paint(X,Y_dict,title,X_len,Y_len,x_label,y_label,marker):
 # BestResultFilePath["all_random"]="../BaseLine/Results/OffLine/google/Random_k_Offline.csv"
 # BestResultFilePath["Mixed-k"]="../BaseLine/Results/OffLine/google/Mixed_k_OffLine.csv"
 # BestResultFilePath["Fair_Rec"]="../BaseLine/Results/OffLine/google/FairRecOffLine.csv"
-# BestResultFilePath["FairSort_offline_Uniform"]="../datasets/results/result_google/FairSortUniformOff8_0.15_0.85.csv"
-# BestResultFilePath["FairSort_offline_Quality_Weighted"]="../datasets/results/result_google/FairSortQualityOff8_0.15_0.85.csv"
 # BestResultFilePath["TFROM_offline_Quality_Weighted"]="../datasets/results/result_google/TFROM/result_quality.csv"
 # BestResultFilePath["TFROM_offline_Uniform"]="../datasets/results/result_google/TFROM/result_Uniform.csv"
+# BestResultFilePath["FairSort_offline_Uniform"]="../datasets/results/result_google/FairSortUniformOff8_0.15_0.85.csv"
+# BestResultFilePath["FairSort_offline_Quality_Weighted"]="../datasets/results/result_google/FairSortQualityOff8_0.15_0.85.csv"
 # resultAnalysis_Online(BestResultFilePath,"","satisfaction_total",5.8,3.2,"K","total recommendation quality","*")
 
 
@@ -165,13 +212,13 @@ def paint(X,Y_dict,title,X_len,Y_len,x_label,y_label,marker):
 # BestResultFilePath={}
 # BestResultFilePath["minimum_exposure"]="../BaseLine/Results/OffLine/google/minimumExposure_OffLine.csv"
 # BestResultFilePath["Top-K"]="../BaseLine/Results/OffLine/google/Top_K_Offline.csv"
-# BestResultFilePath["FairSort_offline_Uniform"]="../datasets/results/result_google/FairSortUniformOff8_0.15_0.85.csv"
-# BestResultFilePath["FairSort_offline_Quality_Weighted"]="../datasets/results/result_google/FairSortQualityOff8_0.15_0.85.csv"
 # BestResultFilePath["all_random"]="../BaseLine/Results/OffLine/google/Random_k_Offline.csv"
 # BestResultFilePath["Mixed-k"]="../BaseLine/Results/OffLine/google/Mixed_k_OffLine.csv"
 # BestResultFilePath["Fair_Rec"]="../BaseLine/Results/OffLine/google/FairRecOffLine.csv"
 # BestResultFilePath["TFROM_offline_Quality_Weighted"]="../datasets/results/result_google/TFROM/result_quality.csv"
 # BestResultFilePath["TFROM_offline_Uniform"]="../datasets/results/result_google/TFROM/result_Uniform.csv"
+# BestResultFilePath["FairSort_offline_Uniform"]="../datasets/results/result_google/FairSortUniformOff8_0.15_0.85.csv"
+# BestResultFilePath["FairSort_offline_Quality_Weighted"]="../datasets/results/result_google/FairSortQualityOff8_0.15_0.85.csv"
 # resultAnalysis_Online(BestResultFilePath,"","satisfaction_var",5.8,3.2,"K","Variance of NDCG","x")
 
 
@@ -182,22 +229,22 @@ def paint(X,Y_dict,title,X_len,Y_len,x_label,y_label,marker):
 # BestResultFilePath["minimum_exposure"]="../BaseLine/Results/OffLine/google/minimumExposure_OffLine.csv"
 # BestResultFilePath["Top-K"]="../BaseLine/Results/OffLine/google/Top_K_Offline.csv"
 # BestResultFilePath["TFROM_offline_Uniform"]="../datasets/results/result_google/TFROM/result_Uniform.csv"
-# BestResultFilePath["FairSort_offline_Uniform"]="../datasets/results/result_google/FairSortUniformOff8_0.15_0.85.csv"
 # BestResultFilePath["all_random"]="../BaseLine/Results/OffLine/google/Random_k_Offline.csv"
 # BestResultFilePath["Mixed-k"]="../BaseLine/Results/OffLine/google/Mixed_k_OffLine.csv"
 # BestResultFilePath["Fair_Rec"]="../BaseLine/Results/OffLine/google/FairRecOffLine.csv"
+# BestResultFilePath["FairSort_offline_Uniform"]="../datasets/results/result_google/FairSortUniformOff8_0.15_0.85.csv"
 #
 # resultAnalysis_Online(BestResultFilePath,"","exposure_var",5.8,3.2,"K","Variance of exposure ","+")
 
 
 # # Google:Result_Analysis:
 #     Variance of the ratio of exposure and relevance
-# pd.read_csv("../datasets/results/result_ctrip/FairSortUniformOff_16_1_0.85.csv")
-# BestResultFilePath={}
-# BestResultFilePath["Top-K"]="../BaseLine/Results/OffLine/google/Top_K_Offline.csv"
-# BestResultFilePath["all_random"]="../BaseLine/Results/OffLine/google/Random_k_Offline.csv"
-# BestResultFilePath["Mixed-k"]="../BaseLine/Results/OffLine/google/Mixed_k_OffLine.csv"
-# BestResultFilePath["Fair_Rec"]="../BaseLine/Results/OffLine/google/FairRecOffLine.csv"
-# BestResultFilePath["FairSort_offline_Quality_Weighted"]="../datasets/results/result_google/FairSortQualityOff8_0.15_0.85.csv"
-# BestResultFilePath["TFROM_offline_Quality_Weighted"]="../datasets/results/result_google/TFROM/result_quality.csv"
-# resultAnalysis_Online(BestResultFilePath,"","exposure_quality_var",5.8,3.2,"K","Variance of the ratio of exposure and relevance","1")
+# # pd.read_csv("../datasets/results/result_ctrip/FairSortUniformOff_16_1_0.85.csv")
+BestResultFilePath={}
+BestResultFilePath["Top-K"]="../BaseLine/Results/OffLine/google/Top_K_Offline.csv"
+BestResultFilePath["all_random"]="../BaseLine/Results/OffLine/google/Random_k_Offline.csv"
+BestResultFilePath["Mixed-k"]="../BaseLine/Results/OffLine/google/Mixed_k_OffLine.csv"
+BestResultFilePath["Fair_Rec"]="../BaseLine/Results/OffLine/google/FairRecOffLine.csv"
+BestResultFilePath["TFROM_offline_Quality_Weighted"]="../datasets/results/result_google/TFROM/result_quality.csv"
+BestResultFilePath["FairSort_offline_Quality_Weighted"]="../datasets/results/result_google/FairSortQualityOff8_0.15_0.85.csv"
+resultAnalysis_Online(BestResultFilePath,"","exposure_quality_var",5.8,3.2,"K","Variance of the ratio of exposure and relevance","1")
--- FairSort_OffLine/ResultAnalysis_OnLine.py
+++ FairSort_OffLine/ResultAnalysis_OnLine.py
@@ -5,7 +5,35 @@ import matplotlib as mpl
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
-
+class DraggableLegend:
+    def __init__(self, legend):
+        self.legend = legend
+        self.gotLegend = False
+        legend.figure.canvas.mpl_connect('motion_notify_event', self.on_motion)
+        legend.figure.canvas.mpl_connect('pick_event', self.on_pick)
+        legend.figure.canvas.mpl_connect('button_release_event', self.on_release)
+        legend.set_picker(self.my_legend_picker)
+    def on_motion(self, evt):
+        if self.gotLegend:
+            dx = evt.x - self.mouse_x
+            dy = evt.y - self.mouse_y
+            loc_in_canvas = self.legend_x + dx, self.legend_y + dy
+            loc_in_norm_axes = self.legend.parent.transAxes.inverted().transform_point(loc_in_canvas)
+            self.legend._loc = tuple(loc_in_norm_axes)
+            self.legend.figure.canvas.draw()
+    def my_legend_picker(self, legend, evt):
+        return self.legend.legendPatch.contains(evt)
+    def on_pick(self, evt):
+        if evt.artist == self.legend:
+            bBox = self.legend.get_window_extent()
+            self.mouse_x = evt.mouseevent.x
+            self.mouse_y = evt.mouseevent.y
+            self.legend_x = bBox.xmin
+            self.legend_y = bBox.ymin
+            self.gotLegend = 1
+    def on_release(self, event):
+        if self.gotLegend:
+            self.gotLegend = False
 
 # Note that even in the OO-style, we use `.pyplot.figure` to create the Figure.
 
@@ -33,11 +61,27 @@ def resultAnalysis_Online(dataSetName,BestResultFilePath,title,indexName,X_len,Y
 def paint(X,Y_dict,title,X_len,Y_len,x_label,y_label):
     fig, ax = plt.subplots(figsize=(X_len, Y_len), layout='constrained')
     for modelName, Y in Y_dict.items():
-        ax.plot(X, Y, label=modelName)  # Plot more data on the axes...
-    ax.set_xlabel(x_label)  # Add an x-label to the axes.
-    ax.set_ylabel(y_label)  # Add a y-label to the axes.
+        if (modelName == "minimum_exposure"):
+            ax.plot(X, Y, label=modelName,  color="darkviolet")  # Plot more data on the axes...
+        elif (modelName == "Top-K"):
+            ax.plot(X, Y, label=modelName,  color="black")  # Plot more data on the axes...
+        elif (modelName == "FairSort_online_Quality_Weighted"):
+            ax.plot(X, Y, label=modelName,  color="blue")  # Plot more data on the axes...
+        elif (modelName == "FairSort_online_Uniform"):
+            ax.plot(X, Y, label=modelName,  color="green")  # Plot more data on the axes...
+        elif (modelName == "all_random"):
+            ax.plot(X, Y, label=modelName, color="purple")  # Plot more data on the axes...
+        elif (modelName == "Mixed-k"):
+            ax.plot(X, Y, label=modelName, color="maroon")  # Plot more data on the axes...
+        elif (modelName == "TFROM_online_Uniform"):
+            ax.plot(X, Y, label=modelName, color="deeppink")  # Plot more data on the axes...
+        elif (modelName == "TFROM_online_Quality_Weighted"):
+            ax.plot(X, Y, label=modelName, color="orange")  # Plot more data on the axes...
+
+    ax.set_xlabel(x_label,fontsize="24")  # Add an x-label to the axes.
+    ax.set_ylabel(y_label,fontsize="24")  # Add a y-label to the axes.
     ax.set_title(title)  # Add a title to the axes.
-    ax.legend()  # Add a legend.
+    DraggableLegend(ax.legend(fontsize="25")) # Add a legend.
     ax.grid(True)
     plt.show()
 
@@ -45,7 +89,7 @@ def paint(X,Y_dict,title,X_len,Y_len,x_label,y_label):
 
 # Ctrip:Result_Analysis:
 #     Total recommendation quality
-
+#
 # BestResultFilePath={}
 # BestResultFilePath["TFROM_online_Quality_Weighted"]="../datasets/results/result_ctrip/TFROM_Dynamic/dynamic_result_Quality.csv"
 # BestResultFilePath["TFROM_online_Uniform"]="../datasets/results/result_ctrip/TFROM_Dynamic/dynamic_result_Uniform.csv"
@@ -66,9 +110,9 @@ def paint(X,Y_dict,title,X_len,Y_len,x_label,y_label):
 # BestResultFilePath["minimum_exposure"]="../BaseLine/Results/OnLine/ctrip/minimumExposure_OnLine.csv"
 # BestResultFilePath["all_random"]="../BaseLine/Results/OnLine/ctrip/Random_k_Online.csv"
 # BestResultFilePath["Mixed-k"]="../BaseLine/Results/OnLine/ctrip/Mixed_k_OnLine.csv"
+# BestResultFilePath["Top-K"]="../BaseLine/Results/OnLine/ctrip/Top_K_Online.csv"
 # BestResultFilePath["FairSort_online_Uniform"]="../datasets/results/result_ctrip/FairSortOnLine/FairSortUniformOn8_1_0.85.csv"
 # BestResultFilePath["FairSort_online_Quality_Weighted"]="../datasets/results/result_ctrip/FairSortOnLine/FairSortQualityOn8_1_0.9.csv"
-# BestResultFilePath["Top-K"]="../BaseLine/Results/OnLine/ctrip/Top_K_Online.csv"
 #
 # resultAnalysis_Online("ctrip",BestResultFilePath,"","satisfaction_var",5.8,3.2,"customer_request","Variance of NDCG")
 
@@ -113,13 +157,13 @@ def paint(X,Y_dict,title,X_len,Y_len,x_label,y_label):
 # BestResultFilePath={}
 # BestResultFilePath["TFROM_online_Quality_Weighted"]="../datasets/results/result_amazon/TFROM_Dynamic/dynamic_result_Quality.csv"
 # BestResultFilePath["TFROM_online_Uniform"]="../datasets/results/result_amazon/TFROM_Dynamic/dynamic_result_Uniform.csv"
-# BestResultFilePath["FairSort_online_Quality_Weighted"]="../datasets/results/result_amazon/FairSortOnLine/FairSortQualityOn1_0.1_0.95.csv"
 # BestResultFilePath["minimum_exposure"]="../BaseLine/Results/OnLine/amazon/minimumExposure_OnLine.csv"
 # BestResultFilePath["Top-K"]="../BaseLine/Results/OnLine/amazon/Top_K_Online.csv"
 # BestResultFilePath["all_random"]="../BaseLine/Results/OnLine/amazon/Random_k_Online.csv"
-# BestResultFilePath["FairSort_online_Uniform"]="../datasets/results/result_amazon/FairSortOnLine/FairSortUniformOn1_0.1_0.95.csv"
 # BestResultFilePath["Mixed-k"]="../BaseLine/Results/OnLine/amazon/Mixed_k_OnLine.csv"
-#
+# BestResultFilePath["FairSort_online_Quality_Weighted"]="../datasets/results/result_amazon/FairSortOnLine/FairSortQualityOn1_0.1_0.95.csv"
+# BestResultFilePath["FairSort_online_Uniform"]="../datasets/results/result_amazon/FairSortOnLine/FairSortUniformOn1_0.1_0.95.csv"
+# #
 # resultAnalysis_Online("amazon",BestResultFilePath,"","satisfaction_var",5.8,3.2,"customer_request","Variance of NDCG")
 
 #Amazon:Result_Analysis:
@@ -156,7 +200,7 @@ def paint(X,Y_dict,title,X_len,Y_len,x_label,y_label):
 # BestResultFilePath["all_random"]="../BaseLine/Results/OnLine/google/Random_k_Online.csv"
 # BestResultFilePath["Mixed-k"]="../BaseLine/Results/OnLine/google/Mixed_k_OnLine.csv"
 # resultAnalysis_Online("google",BestResultFilePath,"","satisfaction_total",5.8,3.2,"customer_request","Total recommendation quality")
-#
+
 #Google:Result_Analysis:
   # Variance of NDCG
 # BestResultFilePath={}
@@ -168,7 +212,7 @@ def paint(X,Y_dict,title,X_len,Y_len,x_label,y_label):
 # BestResultFilePath["TFROM_online_Uniform"]="../datasets/results/result_google/TFROM_Dynamic/dynamic_result_Uniform.csv"
 # BestResultFilePath["FairSort_online_Uniform"]="../datasets/results/result_google/FairSortOnLine/FairSortUniformOn8_0.25_0.85.csv"
 # BestResultFilePath["FairSort_online_Quality_Weighted"]="../datasets/results/result_google/FairSortOnLine/FairSortQualityOn8_0.2_0.85.csv"
-
+#
 # resultAnalysis_Online("google",BestResultFilePath,"","satisfaction_var",5.8,3.2,"customer_request","Variance of NDCG")
 
 # Google:Result_Analysis:
@@ -184,10 +228,10 @@ def paint(X,Y_dict,title,X_len,Y_len,x_label,y_label):
 
 #Google:Result_Analysis:
   # Variance of the ratio of exposure and relevance
-# BestResultFilePath={}
-# BestResultFilePath["TFROM_online_Quality_Weighted"]="../datasets/results/result_google/TFROM_Dynamic/dynamic_result_Quality.csv"
-# BestResultFilePath["Top-K"]="../BaseLine/Results/OnLine/google/Top_K_Online.csv"
-# BestResultFilePath["all_random"]="../BaseLine/Results/OnLine/google/Random_k_Online.csv"
-# BestResultFilePath["Mixed-k"]="../BaseLine/Results/OnLine/google/Mixed_k_OnLine.csv"
-# BestResultFilePath["FairSort_online_Quality_Weighted"]="../datasets/results/result_google/FairSortOnLine/FairSortQualityOn8_0.2_0.85.csv"
-# resultAnalysis_Online("google",BestResultFilePath,"","exposure_quality_var",5.8,3.2,"customer_request","Variance of the ratio of exposure and relevance")
+BestResultFilePath={}
+BestResultFilePath["TFROM_online_Quality_Weighted"]="../datasets/results/result_google/TFROM_Dynamic/dynamic_result_Quality.csv"
+BestResultFilePath["Top-K"]="../BaseLine/Results/OnLine/google/Top_K_Online.csv"
+BestResultFilePath["all_random"]="../BaseLine/Results/OnLine/google/Random_k_Online.csv"
+BestResultFilePath["Mixed-k"]="../BaseLine/Results/OnLine/google/Mixed_k_OnLine.csv"
+BestResultFilePath["FairSort_online_Quality_Weighted"]="../datasets/results/result_google/FairSortOnLine/FairSortQualityOn8_0.2_0.85.csv"
+resultAnalysis_Online("google",BestResultFilePath,"","exposure_quality_var",5.8,3.2,"customer_request","Variance of the ratio of exposure and relevance")
